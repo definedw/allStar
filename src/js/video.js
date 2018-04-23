7b98f26456a02938ba00fecd3ee938f95647df85
sanchuan <416471035@qq.com>
2018-04-23 15:38:39 +0800

    4.23 modify

--- src/js/video.js
+++ src/js/video.js
@@ -2,21 +2,22 @@
 // })
 var videoBtns = $('[data-video-url]');
 var btns = [].slice.call(videoBtns);
-function videoPlay(){
-  console.log($('#videoPlay'));
+
+function videoPlay() {
+    console.log($('#videoPlay'));
 }
 videoBtns.elements.forEach(btn => {
-  $(btn).on($.event.hasTouch ? 'touchstart' : 'click', function(e){
-    var videoUrl = $(this).attr('data-video-url');
-    
-    $.dialog.open({
-      'url':'../tpl/test.html',
-      callback: function(){
-        $('#videoPlay ').html('<source src="'+ videoUrl +'" type="video/mp4"></source>')
-        $("#videoPlay").elements[0].load();
-        $("#videoPlay").elements[0].play();
-      }
-    });
+    $(btn).on($.event.hasTouch ? 'touchstart' : 'click', function(e) {
+        var videoUrl = $(this).attr('data-video-url');
 
-  });
-})
+        $.dialog.open({
+            'url': '../tpl/test.html',
+            callback: function() {
+                $('#videoPlay ').html('<source src="' + videoUrl + '" type="video/mp4"></source>')
+                $("#videoPlay").elements[0].load();
+                $("#videoPlay").elements[0].play();
+            }
+        });
+
+    });
+});
